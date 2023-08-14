2f25206b5a518404df6525299989b943f8d0610a
Jose A. Martinez <[200~jose.alfonso.martinez@gmail.com>
2023-08-14 10:36:45 -0500

    Successfully casted my vote

--- scripts/executeTheVote.js
+++ scripts/executeTheVote.js
@@ -7,6 +7,8 @@ async function main() {
   
   const myGovernorArtifacts = await hre.artifacts.readArtifact("MyGovernor");
   const myGovernorAddress = "0xB26EBf91c820648dC08f8a62C395FfBe3CD70824";
+  const myTokenArtifacts = await hre.artifacts.readArtifact("MyToken");
+  const myTokenAddress = "0x58bEae0AbBAc2b921728f63126d1C4D0e9D2dabd";
   const myAddress = "0x12ff0e981701eA0747058C64936224B8deFCe185";
 
   const proposalId = "8842307623172503925739971616343303302885434907285956958556601970005526381868"; 
@@ -14,10 +16,18 @@ async function main() {
   let wallet = new ethers.Wallet(privateKey, provider);
 
   let myGovernorContract = new ethers.Contract(myGovernorAddress, myGovernorArtifacts.abi, wallet);
+  let myTokenContract = new ethers.Contract(myTokenAddress, myTokenArtifacts.abi, wallet);
 
-  const tx = await myGovernorContract.castVote(proposalId, 1);    
-
-  console.log("Voted successfully... voting ends in one week");
+  //const tx = await myGovernorContract.execute(proposalId);
+     
+  const tx = await myGovernorContract.execute(
+    [myTokenAddress],
+    [0],
+    [myTokenContract.interface.encodeFunctionData("mint", [myAddress, ethers.utils.parseEther("25000")])],
+    "Give the owner more tokens!"
+  );
+  
+  console.log("Executed the proposal successfully... upon minting you will receive the tokens");
   /* This script provided the following output:
 
 */
